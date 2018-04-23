08f0df2a8ec5c02a38ba7b66d7c137121df809bd
AustinDeng <13925510108@163.com>
2018-04-23 19:33:01 +0800

    使用开发工具

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './cake.png'
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div')
+    var btn = document.createElement('button')
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
@@ -14,6 +16,12 @@ function component() {
     myIcon.src = Icon
     element.appendChild(myIcon)
 
+    // innerHTML and onclick not a function!
+    btn.innerHTML = 'Click me and check console!'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
+    
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    }, 
+    // 使用数组 && 复数
+    plugins: [
+        new cleanWebpackPlugin(['dist']),
+        new htmlWebpackPlugin({
+            title: 'Getting Started'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    
     module: {
         rules: [{
                 test: /\.css$/,
