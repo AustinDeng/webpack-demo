42b9c08cfd50b2329eb9a356b8eb7beb3aafc784
AustinDeng <13925510108@163.com>
2018-04-24 16:05:05 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -4,25 +4,35 @@ import Icon from './cake.png'
 import printMe from './print'
 
 function component() {
-    var element = document.createElement('div')
-    var btn = document.createElement('button')
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
-    
-    // 将图像添加到现有的 div
-    var myIcon = new Image()
-    myIcon.src = Icon
-    element.appendChild(myIcon)
+  var element = document.createElement('div')
+  var btn = document.createElement('button')
 
-    // innerHTML and onclick not a function!
-    btn.innerHTML = 'Click me and check console!'
-    btn.onclick = printMe
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
 
-    element.appendChild(btn)
-    
-    return element;
-  }
-  
-document.body.appendChild(component());
+  // 将图像添加到现有的 div
+  var myIcon = new Image()
+  myIcon.src = Icon
+  element.appendChild(myIcon)
+
+  // innerHTML and onclick not a function!
+  btn.innerHTML = 'Click me and check console!'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
+
+  return element;
+}
+
+let element = component() // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js',function(){
+    console.log('Accept the update printMe moudle!')
+    document.body.removeChild(element)
+    element = component() // 重新渲染页面后， component 更新 click 事件处理
+    document.body.appendChild(element)
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const cleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        // print: './src/print.js'
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     }, 
     // 使用数组 && 复数
     plugins: [
         new cleanWebpackPlugin(['dist']),
         new htmlWebpackPlugin({
             title: 'Getting Started'
-        })
+        }),
+        new webpack.NamedModulesPlugin,
+        new webpack.HotModuleReplacementPlugin
     ],
     output: {
         filename: '[name].bundle.js',
